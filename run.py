d536d9ace513241ba00b479a7a43668a0cd9849b
Genos <44347370+Satyavart@users.noreply.github.com>
2020-10-27 21:07:41 +0530

    Update run.py

--- run.py
+++ run.py
@@ -8,25 +8,27 @@ import base64
 from PIL import Image
 
 path = "C:/Classnotes/Dress-Virtual-Trialroom/static/"
-
+frontloc = ''
+bentloc = ''
+backloc = ''
+shirtloc = ''
 fn = []
 app = Flask(__name__)
 
 @app.route("/api",methods=['GET', 'POST'])
-def api_call():
+def api_call(it):
     files = list(request.files)
-    print("{} Files recieved",len(files))
-    it=1
+    print("Files recieved :",len(files))
     os.chdir(path)
     for file in files:
-        print("\nSaving Image No. ", str(it), "/", len(files))
         image = request.files[file]
         filename = werkzeug.utils.secure_filename(image.filename)
+        print(filename)
         abc = filename.split(".")
-        fn[it-1] = str(it) + "." + abc[-1]
+        print(abc)
+        fn.append(str(it) + "." + abc[-1])
         image.save(fn[it-1])
         print("File {} save",it)
-        it = it + 1
     return True
 
 
@@ -36,19 +38,28 @@ def get_response_image(image_path):
         image_base64 = {"base64" : content.decode()}
     return image_base64
 
+def model_test():
+    TestScript.start(frontloc, bentloc, backloc, shirtloc)
+    encoded = get_response_image(path+"out.jpg")
+    return encoded
 
 
 @app.route("/android", methods=['GET', 'POST'])
 def android():
-    if api_call() == True:
+    if api_call(1) == True:
         frontloc = path + fn[0] 
+        print("yes")
+    if api_call(2) == True:
         bentloc = path + fn[1]
+        print("yes")
+    if api_call(3) == True:
         backloc = path + fn[2] 
+        print("yes")
+    if api_call(4) == True:
         shirtloc = path + fn[3]
-        TestScript.start(frontloc, bentloc, backloc, shirtloc)
-        encoded = get_response_image(path+"out.jpg")
-        return jsonify(encoded)
-    return jsonify("error")
+        print("yes")
+    cypher = model_test()
+    return jsonify(cypher)
 
 @app.route("/")
 def home():
