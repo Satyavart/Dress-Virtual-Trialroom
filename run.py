a4d08c3d517134280eb1ab7374c5a68ccd425887
Genos <44347370+Satyavart@users.noreply.github.com>
2020-10-27 20:27:37 +0530

    Update run.py

--- run.py
+++ run.py
@@ -22,12 +22,13 @@ def api_call():
         print("\nSaving Image No. ", str(it), "/", len(files))
         image = request.files[file]
         filename = werkzeug.utils.secure_filename(image.filename)
-        filename.split(".")
-        fn[it-1] = str(it) + "." + filename[-1]
+        abc = filename.split(".")
+        fn[it-1] = str(it) + "." + abc[-1]
         image.save(fn[it-1])
         print("File {} save",it)
         it = it + 1
     return True
+
 
 def get_response_image(image_path):
     with open(image_path,"rb") as file:
